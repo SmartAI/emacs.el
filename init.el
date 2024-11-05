87c39b0e6f1da91134b48e5c7ee2b6e888a63ed8
Min Liu <minliu905@gmail.com>
2024-11-04 19:45:02 -0800

    Org export setting

--- init.el
+++ init.el
@@ -84,6 +84,7 @@
 
 
 ;; transparent
+(set-frame-parameter nil 'alpha-background 95)
 (add-to-list 'default-frame-alist '(alpha-background . 90))
 
 ;; edit
@@ -525,8 +526,9 @@
 	  ))
 
   
-  (define-key global-map (kbd "C-c j")
-	      (lambda () (interactive) (org-capture nil "jj")))
+  ;; Shortcut for capture the journal
+  ;; (define-key global-map (kbd "C-c j")
+  ;; 	      (lambda () (interactive) (org-capture nil "jj")))
 
   
   (min/org-font-setup))
@@ -613,5 +615,219 @@
   "fr" '(counsel-recentf :which-key "recent file")
   )
 
+
+
+;;;; casual suite???
+;;;; TODO:
+
+
+;;; org roam
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/Workspace/orgfiles/org-roam")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  ;; add the time to each entry
+  (setq org-roam-dailies-capture-templates
+	'(("d" "default" entry "* %<%I:%M %p>: %?"
+           :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+  (org-roam-db-autosync-mode))
+
+
+;;; org-babel
+;; TODO
+
+(eval-after-load 'org
+  '(progn
+     ;; active Org-babel languages
+     (org-babel-do-load-languages
+      'org-babel-load-languages
+      '(;; other Babel languages
+        (plantuml . t)
+        (python . t)))
+     (setq org-plantuml-jar-path
+           (expand-file-name "~/.emacs.d/plantuml.jar"))))
+
+;;; Export
+
+;; do not export tags
+(eval-after-load 'ox
+  '(setq org-export-with-tags nil))
+
+
+;; ignore headlines with ignore tag
+(eval-after-load 'ox-extra
+  '(setq ox-extras-activate '(ignore-headlines)))
+
+
+
+(eval-after-load 'ox-latex
+  '(progn
+     (setq org-export-headline-levels 5)
+     (setq org-latex-default-packages-alist
+           '(("AUTO" "inputenc" t ("pdflatex" "lualatex"))
+             ("T1" "fontenc" t ("pdflatex"))
+             ("" "amsmath" t)
+             ("" "amssymb" t)
+             ("" "fontspec" t ("lualatex" "xetex"))
+             ("" "graphicx" t)
+             ("" "longtable" nil)
+             ("" "wrapfig" nil)
+             ("" "rotating" nil)
+             ("normalem" "ulem" t)
+             ("" "capt-of" nil)
+             ("activate={true,nocompatibility},final,tracking=true,kerning=true,spacing=true,factor=1100,stretch=10,shrink=10"
+              "microtype" nil ("pdflatex"))
+             ("activate={true,nocompatibility},final,tracking=true,factor=1100,stretch=10,shrink=10"
+              "microtype" nil ("lualatex"))
+             ("protrusion={true,nocompatibility},final,factor=1100,stretch=10,shrink=10"
+              "microtype" nil ("xelatex"))
+             ("dvipsnames,svgnames" "xcolor" nil)
+             ("colorlinks=true, linkcolor=DarkBlue, citecolor=BrickRed, urlcolor=DarkGreen" "hyperref" nil)))))
+
+
+(eval-after-load 'ox
+  '(progn
+     (add-to-list 'org-latex-classes
+                  '("koma-letter" "\\documentclass[11pt]{scrletter}"
+                    ("\\section{%s}" . "\\section*{%s}")
+                    ("\\subsection{%s}" . "\\subsection*{%s}")
+                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                    ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                    ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+     (add-to-list 'org-latex-classes
+                  '("koma-article" "\\documentclass[11pt]{scrartcl}"
+                    ("\\section{%s}" . "\\section*{%s}")
+                    ("\\subsection{%s}" . "\\subsection*{%s}")
+                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                    ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                    ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+     (add-to-list 'org-latex-classes
+                  '("koma-report" "\\documentclass[11pt]{scrreprt}"
+                    ("\\part{%s}" . "\\part*{%s}")
+                    ("\\chapter{%s}" . "\\chapter*{%s}")
+                    ("\\section{%s}" . "\\section*{%s}")
+                    ("\\subsection{%s}" . "\\subsection*{%s}")
+                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+     (add-to-list 'org-latex-classes
+                  '("koma-book" "\\documentclass[11pt]{scrbook}"
+                    ("\\part{%s}" . "\\part*{%s}")
+                    ("\\chapter{%s}" . "\\chapter*{%s}")
+                    ("\\section{%s}" . "\\section*{%s}")
+                    ("\\subsection{%s}" . "\\subsection*{%s}")
+                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+     (setq org-latex-default-class "koma-article")))
+
+
+(eval-after-load 'ox-latex
+  '(setq org-latex-src-block-backend 'engraved))
+
+(use-package engrave-faces
+  :config
+  (setq engrave-faces-themes
+        '((default .
+		   (;; faces.el --- excluding: bold, italic, bold-italic, underline, and some others
+		    (default                             :short "default"             :slug "D"   :foreground "#000000" :background "#ffffff" :family "Monospace")
+		    (variable-pitch                      :short "var-pitch"           :slug "vp"  :foreground "#000000"                       :family "Sans Serif")
+		    (shadow                              :short "shadow"              :slug "h"   :foreground "#7f7f7f")
+		    (success                             :short "success"             :slug "sc"  :foreground "#228b22" :weight bold)
+		    (warning                             :short "warning"             :slug "w"   :foreground "#ff8e00" :weight bold)
+		    (error                               :short "error"               :slug "e"   :foreground "#ff0000" :weight bold)
+		    (link                                :short "link"                :slug "l"   :foreground "#ff0000")
+		    (link-visited                        :short "link"                :slug "lv"  :foreground "#ff0000")
+		    (highlight                           :short "link"                :slug "hi"  :foreground "#ff0000")
+		    ;; font-lock.el
+		    (font-lock-comment-face              :short "fl-comment"          :slug "c"   :foreground "#b22222")
+		    (font-lock-comment-delimiter-face    :short "fl-comment-delim"    :slug "cd"  :foreground "#b22222")
+		    (font-lock-string-face               :short "fl-string"           :slug "s"   :foreground "#8b2252")
+		    (font-lock-doc-face                  :short "fl-doc"              :slug "d"   :foreground "#8b2252")
+		    (font-lock-doc-markup-face           :short "fl-doc-markup"       :slug "m"   :foreground "#008b8b")
+		    (font-lock-keyword-face              :short "fl-keyword"          :slug "k"   :foreground "#9370db")
+		    (font-lock-builtin-face              :short "fl-builtin"          :slug "b"   :foreground "#483d8b")
+		    (font-lock-function-name-face        :short "fl-function"         :slug "f"   :foreground "#0000ff")
+		    (font-lock-variable-name-face        :short "fl-variable"         :slug "v"   :foreground "#a0522d")
+		    (font-lock-type-face                 :short "fl-type"             :slug "t"   :foreground "#228b22")
+		    (font-lock-constant-face             :short "fl-constant"         :slug "o"   :foreground "#008b8b")
+		    (font-lock-warning-face              :short "fl-warning"          :slug "wr"  :foreground "#ff0000" :weight bold)
+		    (font-lock-negation-char-face        :short "fl-neg-char"         :slug "nc")
+		    (font-lock-preprocessor-face         :short "fl-preprocessor"     :slug "pp"  :foreground "#483d8b")
+		    (font-lock-regexp-grouping-construct :short "fl-regexp"           :slug "rc"                        :weight bold)
+		    (font-lock-regexp-grouping-backslash :short "fl-regexp-backslash" :slug "rb"                        :weight bold)
+		    ;; org-faces.el
+		    (org-block                           :short "org-block"           :slug "ob") ; forcing no background is preferable
+		    (org-block-begin-line                :short "org-block-begin"     :slug "obb") ; forcing no background is preferable
+		    (org-block-end-line                  :short "org-block-end"       :slug "obe") ; forcing no background is preferable
+		    ;; outlines
+		    (outline-1                           :short "outline-1"           :slug "Oa"  :foreground "#0000ff")
+		    (outline-2                           :short "outline-2"           :slug "Ob"  :foreground "#a0522d")
+		    (outline-3                           :short "outline-3"           :slug "Oc"  :foreground "#a020f0")
+		    (outline-4                           :short "outline-4"           :slug "Od"  :foreground "#b22222")
+		    (outline-5                           :short "outline-5"           :slug "Oe"  :foreground "#228b22")
+		    (outline-6                           :short "outline-6"           :slug "Of"  :foreground "#008b8b")
+		    (outline-7                           :short "outline-7"           :slug "Og"  :foreground "#483d8b")
+		    (outline-8                           :short "outline-8"           :slug "Oh"  :foreground "#8b2252")
+		    ;; highlight-numbers.el
+		    (highlight-numbers-number            :short "hl-number"           :slug "hn"  :foreground "#008b8b")
+		    ;; highlight-quoted.el
+		    (highlight-quoted-quote              :short "hl-qquote"           :slug "hq"  :foreground "#9370db")
+		    (highlight-quoted-symbol             :short "hl-qsymbol"          :slug "hs"  :foreground "#008b8b")
+		    ;; rainbow-delimiters.el
+		    (rainbow-delimiters-depth-1-face     :short "rd-1"                :slug "rda" :foreground "#707183")
+		    (rainbow-delimiters-depth-2-face     :short "rd-2"                :slug "rdb" :foreground "#7388d6")
+		    (rainbow-delimiters-depth-3-face     :short "rd-3"                :slug "rdc" :foreground "#909183")
+		    (rainbow-delimiters-depth-4-face     :short "rd-4"                :slug "rdd" :foreground "#709870")
+		    (rainbow-delimiters-depth-5-face     :short "rd-5"                :slug "rde" :foreground "#907373")
+		    (rainbow-delimiters-depth-6-face     :short "rd-6"                :slug "rdf" :foreground "#6276ba")
+		    (rainbow-delimiters-depth-7-face     :short "rd-7"                :slug "rdg" :foreground "#858580")
+		    (rainbow-delimiters-depth-8-face     :short "rd-8"                :slug "rdh" :foreground "#80a880")
+		    (rainbow-delimiters-depth-9-face     :short "rd-9"                :slug "rdi" :foreground "#887070")
+		    ;; Diffs
+		    (diff-added       :short "diff-added"       :slug  "diffa"  :foreground "#4F894C")
+		    (diff-changed     :short "diff-changed"     :slug  "diffc"  :foreground "#842879")
+		    (diff-context     :short "diff-context"     :slug  "diffco" :foreground "#525866")
+		    (diff-removed     :short "diff-removed"     :slug  "diffr"  :foreground "#99324B")
+		    (diff-header      :short "diff-header"      :slug  "diffh"  :foreground "#398EAC")
+		    (diff-file-header :short "diff-file-header" :slug  "difffh" :foreground "#3B6EA8")
+		    (diff-hunk-header :short "diff-hunk-header" :slug  "diffhh" :foreground "#842879")
+		    )))))
+
+
+
+;; Narrow editing
+(use-package olivetti
+  :ensure t
+  :config
+  (setq-default olivetti-body-width 130)
+  (add-hook 'mixed-pitch-mode-hook  
+            (lambda () 
+              (setq-local olivetti-body-width 90)))
+  
+  ;; auto enable olivetti-mode
+  (defvar auto-olivetti-modes
+    '(text-mode prog-mode helpful-mode ibuffer-mode image-mode)
+    "List of modes that should use `olivetti-mode'.")
+
+  (defun enable-olivetti-mode-maybe ()
+    "Enable `olivetti-mode' if the current major mode is derived from any mode in `auto-olivetti-modes'."
+    (when (seq-some (lambda (mode) (derived-mode-p mode)) auto-olivetti-modes)
+      (olivetti-mode 1)))
+
+  ;; Add hooks 
+  (add-hook 'after-change-major-mode-hook #'enable-olivetti-mode-maybe))
 
 
